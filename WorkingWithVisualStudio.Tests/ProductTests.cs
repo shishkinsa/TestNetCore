07e4a23ceb61ef5830b34850c46dadf260d6486f
ShishkinSA <shishkin.s.a@gmail.com>
2019-11-14 00:25:05 +0300

    init proogect

---
+++ WorkingWithVisualStudio.Tests/ProductTests.cs
@@ -0,0 +1,34 @@
+﻿using WorkingWithVisualStudio.Models;
+using Xunit;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class ProductTests // Имя класса то, что подвергается тестированию
+    {
+        /// <summary>
+        /// arrange/act/assert - /A/A/A
+        /// </summary>
+
+        [Fact]
+        public void CanChangeProductName() // Имя метода описывает то, что делает тест
+        {
+            // Организация
+            var p = new Product {Name = "Test", Price = 100M};
+            // Действие
+            p.Name = "New Name";
+            // Утвеждение
+            Assert.Equal("New Name", p.Name);
+        }
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            // Организация
+            var p = new Product { Name = "Test", Price = 100M };
+            // Действие
+            p.Price = 200M;
+            // Утверждение
+            Assert.Equal(100M, p.Price);
+
+        }
+    }
+}
